6d545038fcef52a09c21d256f090ae897ce52d17
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2024-03-19 20:53:26 -0500

    adds product functionality in users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
+const { propfind } = require('.');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  factor++;
+  product = product*factor;
+  res.send(`Product is ${product}`);
 });
+
+var product = 1;
+var factor = 1;
 
 module.exports = router;
